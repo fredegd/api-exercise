66ee13924a638702ed9a93fc6dfcb3e9bc43d8d9
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2023-05-26 16:04:13 +0200

    adding Modal for the show user Info

--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
-import Toast from "react-bootstrap/Toast";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function PostListItem({ post }) {
   const commentUrl = `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`;
   const userUrl = `https://jsonplaceholder.typicode.com/users/${post.userId}`;
   const [comments, setComments] = useState();
-  const [user, setUser] = useState();
-  
-  const [show, toggleShow] = useState(false);
-
+  const [user, setuser] = useState();
+  const [showModal, setShowModal] = useState(false);
 
   const deletePost = () =>{
     axios.delete(
@@ -23,7 +20,7 @@ export default function PostListItem({ post }) {
   }
 
   const getComments = () => {
-    comments? setComments() : axios
+    comments ? setComments() : axios
       .get(commentUrl)
       .then((response) => {
         console.log(response.data);
@@ -36,41 +33,49 @@ export default function PostListItem({ post }) {
   };
 
   const getUser = () => {
-   user ?  setUser("")    : axios
-      .get(userUrl)
-      .then((response) => {
-    setUser(response.data);
-      console.log(user.show)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
    
+      axios
+        .get(userUrl)
+        .then((response) => {
+          setuser(response.data);
+          setShowModal(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   };
 
   return (
     <div>
-      
       <h1>{post.title}</h1>
       <p>{post.body}</p>
       <button>Edit</button>
       <button onClick={deletePost}>Delete</button>
-      <button onClick={getComments}>{comments?"Hide":"Show"} Comments</button>
-      <button onClick={getUser}>{user?"Hide":"Show"} User</button>
+      <button onClick={getComments}>{comments ? "Hide" : "Show"} Comments</button>
+      <button onClick={getUser}>Show User</button>
 
       {user && (
-        <div className="user-section">
-           <h3>User Information</h3>
-           <p>Name: {user.name}</p>
-           <p>Email: {user.email}</p>
-           <p>Website: {user.website}</p>
-        </div>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>User Information</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Website: {user.website}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={getUser}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       )}
 
       {comments && (
         <div className="comment-section">
           <ul>
-          {comments.map((comment) => (
+            {comments.map((comment) => (
               <li key={comment.id}>
                 <h4>{comment.name}</h4>
                 <p>{comment.body}</p>
